1feb73a3884494626a9f3cf4f7aab7c8b01265bf
sarathkella <sarathchandra231@gmail.com>
2019-01-12 15:09:52 +0530

    some changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class book {
+class Book {
     constructor(title, author, isbn){
         this.title = title;
         this.author = author;
@@ -37,10 +37,59 @@ class book {
 
          list.appendChild(row);
      }
+
+    static deleteBook(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, classname){
+           const div = document.createElement('div');
+           div.className= `alert alert-${classname}`;
+           div.appendChild(document.createTextNode(message));
+           const container = document.querySelector('.container');
+           const form = document.querySelector('#book-form');
+           container.insertBefore(div, form);
+           setTimeout(()=>document.querySelector('alert').remove(), 2000)
+    }
+
+     static clearFields(){
+         document.querySelector('#title').value = '';
+         document.querySelector('#author').value = '';
+         document.querySelector('#isbn').value = '';
+     }
+
  }
 
  document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 
+ document.querySelector('#book-form').addEventListener('submit', (e) =>{
 
- 
+    e.preventDefault(); 
+     
+     const title = document.querySelector('#title').value;
+     const author = document.querySelector('#author').value;
+     const isbn = document.querySelector('#isbn').value;
+
+    if(title === '' || author === '' || isbn === ''){
+       UI.showAlert('fill all the fields', 'danger');
+    }
+    else {
+        const book = new Book(title, author, isbn);
+        UI.AddBookToList(book);
+        UI.showAlert('Book added', 'success')
+        UI.clearFields();
+    }
+ });
+
+
+ document.querySelector('#book-list').addEventListener('click', (e) => {
+          UI.deleteBook(e.target);
+          UI.showAlert('Book removed','success');
+ });
+
+
+
+  
